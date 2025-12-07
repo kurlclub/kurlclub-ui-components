ab7aff77a69b8996a7aff912f379a6979cd8bb09
hafizn07 <hafiznjh007@gmail.com>
2025-12-07 23:50:36 +0530

    chore: version bump

--- src/Introduction.stories.tsx
+++ src/Introduction.stories.tsx
@@ -455,7 +455,7 @@ export default {
             { label: 'Tailwind', value: '4.x' },
             { label: 'Radix UI', value: 'Latest' },
             { label: 'Storybook', value: '10.x' },
-            { label: 'Version', value: 'v1.0.2' },
+            { label: 'Version', value: 'v1.0.3' },
           ].map((tech) => (
             <div
               key={tech.label}
